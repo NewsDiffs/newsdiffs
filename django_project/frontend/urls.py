9792ae09c9b1fff4afa07d3fd5465ea5655015de
Carl G <carl.gieringer@gmail.com>
2017-11-23 10:47:27 -0600

    Fix mismatch between using article filename vs. url

--- django_project/frontend/urls.py
+++ django_project/frontend/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import url
 
 urlpatterns = [
   # These are deprecated, and meant to preserve legacy URLs:
-  url(r'^diffview/$', 'frontend.views.old_diffview'),
-  url(r'^article-history/$', 'frontend.views.article_history', name='article_history'),
+  url(r'^diffview/$', 'frontend.views.old_diff_view'),
 
   # These are current:
   url(r'^upvote/$', 'frontend.views.upvote', name='upvote'),
-  url(r'^diff/(?P<vid1>\d+)/(?P<vid2>\d+)/(?P<urlarg>.*)$', 'frontend.views.diffview', name='diffview'),
+  url(r'^diff/(?P<vid1>\d+)/(?P<vid2>\d+)/(?P<diff_url>.*)$', 'frontend.views.diff_view', name='diff_view'),
   url(r'^about/$', 'frontend.views.about', name='about'),
   url(r'^browse/$', 'frontend.views.browse', name='browse'),
   url(r'^browse/(.*)$', 'frontend.views.browse', name='browse'),
@@ -16,8 +15,11 @@ urlpatterns = [
   url(r'^examples/$', 'frontend.views.examples', name='examples'),
   url(r'^subscribe/$', 'frontend.views.subscribe', name='subscribe'),
   url(r'^press/$', 'frontend.views.press', name='press'),
-  url(r'^feed/article-history/(.*)$', 'frontend.views.article_history_feed', name='article_history_feed'),
-  url(r'^article-history/(?P<urlarg>.*)$', 'frontend.views.article_history', name='article_history'),
+  url(r'^feed/article-history/(?P<history_url>.*)$', 'frontend.views.article_history_feed', name='article_history_feed'),
+  url(r'^article-history/(?P<history_url>.*)$', 'frontend.views.article_history', name='article_history'),
   url(r'^json/view/(?P<vid>\d+)/?$', 'frontend.views.json_view'),
   url(r'^$', 'frontend.views.front', name='root'),
+
+  url(r'^search-url/$', 'frontend.views.article_history_by_url',
+      name='article_history_by_url'),
 ]
--- django_project/frontend/views.py
+++ django_project/frontend/views.py
@@ -7,7 +7,7 @@ import re
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
-from django.template import Context, RequestContext, loader
+from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 
 from frontend import models
@@ -69,7 +69,7 @@ def get_last_update(source):
 
 
 def get_articles(source=None, distance=0):
-    articles = []
+
     rx = re.compile(r'^https?://(?:[^/]*\.)%s/' % source if source else '')
 
     pagelength = datetime.timedelta(days=1)
@@ -121,18 +121,19 @@ def get_articles(source=None, distance=0):
         v.article = a
         article_dict.setdefault(v.article, []).append(v)
 
+    articles = []
     for article, versions in article_dict.items():
         url = article.url
         if not rx.match(url):
             logger.info('URL did not pass filter: %s', url)
             continue
-        if 'blogs.nytimes.com' in url: #XXX temporary
+        if 'blogs.nytimes.com' in url:  # XXX temporary
             logger.info('Skipping blogs.nytimes.com URL: %s', url)
             continue
         if len(versions) < 2:
             continue
-        rowinfo = get_rowinfo(article, versions)
-        articles.append((article, versions[-1], rowinfo))
+        version_info = get_version_info(article, versions)
+        articles.append((article, versions[-1], version_info))
     articles.sort(key=lambda x: x[-1][0][1].date, reverse=True)
     return articles
 
@@ -201,7 +202,7 @@ def feed(request, source=''):
             mimetype='application/atom+xml')
 
 
-def old_diffview(request):
+def old_diff_view(request):
     """Support for legacy diff urls"""
     url = request.REQUEST.get('url')
     v1tag = request.REQUEST.get('v1')
@@ -220,15 +221,13 @@ def old_diffview(request):
     except Article.DoesNotExist:
         raise Http404
 
-    return redirect(reverse('diffview', kwargs=dict(vid1=v1.id,
-                                                    vid2=v2.id,
-                                                    urlarg=article.filename())),
-                    permanent=True)
+    view_kwargs = dict(vid1=v1.id,
+                       vid2=v2.id,
+                       diff_url=article.url)
+    return redirect(reverse(diff_view, kwargs=view_kwargs), permanent=True)
 
 
-def diffview(request, vid1, vid2, urlarg):
-    # urlarg is unused, and only for readability
-    # Could be strict and enforce urlarg == article.filename()
+def diff_view(request, vid1, vid2, diff_url):
     try:
         v1 = Version.objects.get(id=int(vid1))
         v2 = Version.objects.get(id=int(vid2))
@@ -261,25 +260,25 @@ def diffview(request, vid1, vid2, urlarg):
     if any(x is None for x in texts):
         raise Exception('missing text for some version')
 
-    links = []
+    diff_hrefs = []
     for i in range(2):
         if all(x[i] for x in adjacent_versions):
-            diffl = reverse('diffview', kwargs=dict(vid1=adjacent_versions[0][i].id,
-                                                    vid2=adjacent_versions[1][i].id,
-                                                    urlarg=article.filename()))
-            links.append(diffl)
+            kwargs = dict(vid1=adjacent_versions[0][i].id,
+                          vid2=adjacent_versions[1][i].id,
+                          diff_url=diff_url)
+            diff_href = reverse('diff_view', kwargs=kwargs)
+            diff_hrefs.append(diff_href)
         else:
-            links.append('')
+            diff_hrefs.append('')
 
-    return render_to_response('diffview.html', {
+    return render_to_response('diff_view.html', {
         'title': title,
         'date1': dates[0],
         'date2': dates[1],
         'text1': texts[0],
         'text2': texts[1],
-        'prev': links[0],
-        'next': links[1],
-        'article_shorturl': article.filename(),
+        'prev': diff_hrefs[0],
+        'next': diff_hrefs[1],
         'article_url': article.url,
         'v1': v1,
         'v2': v2,
@@ -287,24 +286,23 @@ def diffview(request, vid1, vid2, urlarg):
     })
 
 
-def get_rowinfo(article, version_lst=None):
-    if version_lst is None:
-        version_lst = article.versions()
-    rowinfo = []
-    lastv = None
-    urlarg = article.filename()
-    for version in version_lst:
-        date = version.date
-        if lastv is None:
-            diffl = ''
+def get_version_info(article, versions=None):
+    if versions is None:
+        versions = article.versions()
+    version_info = []
+    last_version = None
+    for version in versions:
+        if last_version is None:
+            diff_href = ''
         else:
-            diffl = reverse('diffview', kwargs=dict(vid1=lastv.id,
-                                                    vid2=version.id,
-                                                    urlarg=urlarg))
-        rowinfo.append((diffl, version))
-        lastv = version
-    rowinfo.reverse()
-    return rowinfo
+            kwargs = dict(vid1=last_version.id,
+                          vid2=version.id,
+                          diff_url=article.url)
+            diff_href = reverse('diff_view', kwargs=kwargs)
+        version_info.append((diff_href, version))
+        last_version = version
+    version_info.reverse()
+    return version_info
 
 
 def prepend_http(url):
@@ -313,7 +311,7 @@ def prepend_http(url):
     url may look like
 
     www.nytimes.com
-    https:/www.nytimes.com    <- because double slashes get stripped
+    https:/www.nytimes.com  <- because Apache collapses adjacent slashes
     http://www.nytimes.com
     """
     components = url.split('/', 2)
@@ -340,71 +338,69 @@ def decode_scheme_colon(url):
     return re.sub('http(s?)%(25)*3A', 'http\g<1>:', url)
 
 
-def article_history(request, urlarg=''):
-    url = request.REQUEST.get('url') # this is the deprecated interface.
-    if url is None:
-        url = urlarg
-    if len(url) == 0:
-        return HttpResponseRedirect(reverse(front))
+def article_history(request, history_url):
 
-    url = url_util.remove_query_params(url)  #For if user copy-pastes from news site
-
-    url = prepend_http(url)
-
-    # This is a hack to deal with unicode passed in the URL.
-    # Otherwise gives an error, since our table character set is latin1. (Why not encode the table as unicode?)
-    url = url.encode('ascii', 'ignore')
+    normal_url = url_util.remove_query_params(history_url)  # For if user copy-pastes from news site
+    normal_url = prepend_http(normal_url)
 
     # Give an error on urls with the wrong hostname without hitting the
     # database.  These queries are usually spam.
-    domain = url_util.get_url_domain(url)
+    domain = url_util.get_url_domain(normal_url)
     if not is_valid_domain(domain):
-        # Should really tell the users that it is missing
-        return render_to_response('article_history_missing.html', {'url': url})
+        logger.debug('Unsupported domain for URL: %s', history_url)
+        return render_to_response('article_history_missing.html', {
+            'history_url': normal_url,
+            'message': 'NewsDiffs currently does not support this domain: %s' % domain
+        })
 
-    decoded_url = decode_scheme_colon(url)
+    decoded_url = decode_scheme_colon(normal_url)
     try:
         try:
             article = Article.objects.get(url=decoded_url)
         except Article.DoesNotExist:
             article = Article.objects.get(url=swap_http_https(decoded_url))
     except Article.DoesNotExist:
-        try:
-            return render_to_response('article_history_missing.html', {'url': decoded_url})
-        except (TypeError, ValueError):
-            # bug in django + mod_rewrite can cause this. =/
-            return HttpResponse('Bug!')
+        return render_to_response('article_history_missing.html',
+                                  {'url': decoded_url})
 
-    if len(urlarg) == 0:
-        return HttpResponseRedirect(reverse(article_history, args=[article.filename()]))
-
-    rowinfo = get_rowinfo(article)
-    return render_to_response('article_history.html', {'article':article,
-                                                       'versions':rowinfo,
-            'display_search_banner': came_from_search_engine(request),
-                                                       })
+    version_info = get_version_info(article)
+    return render_to_response('article_history.html', {
+        'article': article,
+        'versions': version_info,
+        'display_search_banner': came_from_search_engine(request),
+    })
 
 
-def article_history_feed(request, url=''):
+def article_history_feed(request, url):
     url = prepend_http(url)
     article = get_object_or_404(Article, url=url)
-    rowinfo = get_rowinfo(article)
-    return render_to_response('article_history.xml',
-                              { 'article': article,
-                                'versions': rowinfo,
-                                'request': request,
-                                },
+    version_info = get_version_info(article)
+    args = {
+        'article': article,
+        'versions': version_info,
+        'request': request,
+    }
+    return render_to_response('article_history.xml', args=args,
                               context_instance=RequestContext(request),
                               mimetype='application/atom+xml')
+
+
+def article_history_by_url(request):
+    url = request.REQUEST.get('url')
+    if not url:
+        return redirect(reverse(browse))
+    else:
+        view_kwargs = dict(history_url=url)
+        return redirect(reverse(article_history, kwargs=view_kwargs))
 
 
 def json_view(request, vid):
     version = get_object_or_404(Version, id=int(vid))
     data = dict(
         title=version.title,
-        byline = version.byline,
-        date = version.date.isoformat(),
-        text = version.text(),
+        byline=version.byline,
+        date=version.date.isoformat(),
+        text=version.text(),
         )
     return HttpResponse(json.dumps(data), mimetype="application/json")
 
@@ -415,7 +411,9 @@ def upvote(request):
     diff_v2 = request.REQUEST.get('diff_v2')
     remote_ip = request.META.get('REMOTE_ADDR')
     article_id = Article.objects.get(url=article_url).id
-    models.Upvote(article_id=article_id, diff_v1=diff_v1, diff_v2=diff_v2, creation_time=datetime.datetime.utcnow(), upvoter_ip=remote_ip).save()
+    models.Upvote(article_id=article_id, diff_v1=diff_v1, diff_v2=diff_v2,
+                  creation_time=datetime.datetime.utcnow(),
+                  upvoter_ip=remote_ip).save()
     return render_to_response('upvote.html')
 
 
